4bcdcaf6c073bea7e56ce0622385ed1e7aed8d83
Rahul NS <rahul9380767545@gmail.com>
2025-01-12 23:51:19 +0530

    Add routing to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import { Dashboard } from "./pages/Dashboard"
+import { Signin } from "./pages/Signin"
 import { Signup } from "./pages/Signup"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return <Signup />
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+
+      {/* Redirect incorrect capitalization to lowercase */}
+      <Route path="/Signup" element={<Navigate to="/signup" replace />} />
+      <Route path="/Signin" element={<Navigate to="/signin" replace />} />
+
+      {/* Optional: Catch-all route for 404 */}
+      <Route path="*" element={<div className="flex justify-center 
+      items-center h-screen font-bold">
+        404 - Page Not Found
+      </div>} />
+
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
